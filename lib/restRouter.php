5989b4ed0c942d60300da37c1209a0e4c265e7c8
David Blanchard <dpjb78@gmail.com>
2016-09-27 18:46:44 +0200

    Try to make a REAL router
    Try to make a REAL router

--- lib/restRouter.php
+++ lib/restRouter.php
@@ -17,9 +17,11 @@ class RestRouter
     
     //put your code here
     private $application = null;
-    private $apiName = '';    
+    private $apiName = '';
+	private $className = '';
     private $baseNamespace = '';
     private $apiFileName = '';
+	private $parameter = '';
 
     public function __construct(RestApplication $app)
     {
@@ -33,12 +35,13 @@ class RestRouter
 
         $nsParts = explode('\\', __NAMESPACE__);
         $this->baseNamespace = array_shift($nsParts);
-        
-        $qstring = str_replace('/api/', '', REQUEST_URI);
-        
+
+		$qstring = str_replace('/api/', '', REQUEST_URI);
         $qParts = explode('/', $qstring);
-        
-        $this->apiName = $qParts[0];
+		$$this->parameter = array_shift($qParts)+0;
+ 
+		$this->apiName = preg_replace('/[^a-z0-9_]+/i','', array_shift($qParts));
+        $this->className = ucfirst($this->apiName);
         
         $this->apiFileName = DOCUMENT_ROOT . 'app' . DIRECTORY_SEPARATOR . 'rest' . DIRECTORY_SEPARATOR . $this->apiName . '.class.php';
         
@@ -47,12 +50,39 @@ class RestRouter
 
     public function dispatch()
     {
-        $fqObject = $this->baseNamespace . '\\Rest\\' . ucfirst($this->apiName);
+		$data = [];
+        $method = REQUEST_METHOD;
+
+        $fqObject = $this->baseNamespace . '\\Rest\\' . $this->className;
 
         include $this->apiFileName;
 
         $instance = new $fqObject($this->application);
         
-        $instance->render();
+        $request_body = file_get_contents('php://input');
+        if(!empty($request_body)) {
+            $data = json_decode($request_body, true);
+        }
+        
+        $params = [];
+        if(count($data) > 0) {
+            $params = array_values($data);
+            if($this->parameter !== null) {
+                array_unshift($params, $this->parameter);
+            }
+        } else {
+            if($this->parameter !== null) {
+                $params = [$this->parameter];
+            }
+        }
+            
+        $ref = new \ReflectionMethod($instance, $method);
+        if(count($params) > 0) {
+            $ref->invokeArgs($instance, $params);
+        } else {
+            $ref->invoke($instance);
+        }
+        
+        $this->response->sendJsonData();		
     }
 }
