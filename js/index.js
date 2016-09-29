594827ff5f937860e0b81d7ac12f086c57c924c8
david <dblanchard1@bbox.fr>
2016-09-29 14:26:59 +0200

    A Nice JS API

--- js/index.js
+++ js/index.js
@@ -4,11 +4,67 @@
  * and open the template in the editor.
  */
 
+var getCollection = function() {
+    coll.fetch(function(data) {
+        var result = '<ol>'
+        data = data.collection
+
+        for(var i = 0; i < data.length; i++) {
+            var duration = data[i].duration
+
+            var minutes = Math.floor(duration / 60)
+            var seconds = duration - (minutes * 60)
+            duration = minutes + ':' + ('00' + seconds).toString().slice(-2)
+
+            result += '<li><a href="javascript:pl.addTrack(' + data[i].id + ')" ><img src="/css/images/add.png" /></a>&nbsp;' + data[i].artist + ' - ' + data[i].title + ' (' + duration + ')'  + '</li>'
+        }
+        result += '</ol>'
+        
+        var div = document.getElementById('collection')
+        if(div !== undefined) {
+            div.innerHTML = result
+        }
+    })
+    
+}
+
+var getUserFavorites = function() {
+    pl.getFavorites(function(data) {
+        var result = '<ol>'
+        data = data.playlist
+        if(data[0].artist === null && data[0].title === null) {
+            result = 'La playlist est vide'
+        } else {
+            for(var i = 0; i < data.length; i++) {
+                var duration = data[i].duration
+                
+                var minutes = Math.floor(duration / 60)
+                var seconds = duration - (minutes * 60)
+                duration = minutes + ':' + ('00' + seconds).toString().slice(-2)
+            
+                result += '<li><a href="javascript:pl.removeTrack(' + data[i].id + ')" ><img src="/css/images/delete.png" /></a>&nbsp;' + data[i].artist + ' - ' + data[i].title + ' (' + duration + ')'  + '</li>'
+            }
+            result += '</ol>'
+        }
+        
+        var div = document.getElementById('playlist')
+        if(div !== undefined) {
+            div.innerHTML = result
+        }
+    })
+    
+}
+
 var currentUser = 1
-var sl = new SoundLib('sl')
+var usr = new SoundLib.User(currentUser)
+var pl = new SoundLib.Playlist(currentUser)
+var coll = new SoundLib.Collection()
+pl.afterAddTrack = getUserFavorites
+pl.afterRemoveTrack = getUserFavorites
 
 SoundLib.DOM.ready(function() {
-    sl.getUserInfo(this.currentUser)
-    sl.getCollection()
-    sl.getUserFavorites()
+    
+    usr.getInfo()
+    getCollection()
+    getUserFavorites()
 })
